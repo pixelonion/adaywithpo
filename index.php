2efd1f0dbf24a864017450d177fd609e6350be1a
SJ <sj@pixelonion.com>
2017-10-20 10:52:51 +0800

    First commit.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>A day with Pixel Onion</title>
+</head>
+<body>
+  <header>
+    <div>Pixel Onion logo</div>
+    <div>Site logo</div>
+  </header>
+  <section>
+    Main content
+  </section>
+  <footer>
+    Footer
+  </footer>
+</body>
+</html>
