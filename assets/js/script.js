8cf967c260eed947b2950ce832375ce4b2a5e442
1902691-TeoLiXien <1902691@sit.singaporetech.edu.sg>
2021-09-09 02:52:13 +0800

    completed baby name finder website

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+// onmousein and onmouseout
+function hoverFacebook(obj) {
+    obj.src = "assets/images/ic-facebook-active-24.png";
+}
+
+function hoverFacebookout(obj){
+    obj.src = "assets/images/ic-facebook-inactive-24.png";
+}
+
+function hoverLinkedin(obj) {
+    obj.src = "assets/images/ic-linkedin-active-24.png";
+}
+
+function hoverLinkedinout(obj){
+    obj.src = "assets/images/ic-linkedin-inactive-24.png";
+}
+
+function hovertwitter(obj) {
+    obj.src = "assets/images/ic-twitter-active-24.png";
+}
+
+function hovertwitterout(obj){
+    obj.src = "assets/images/ic-twitter-inactive-24.png";
+}
